2f848449f40d4be6f8270f2675ba937e99b469b3
Grigoris Liaskos <grigorisliaskos911@gmail.com>
2024-04-16 19:38:25 +0300

    check duplicate email

--- src/app/components/user-registration/user-registration.component.ts
+++ src/app/components/user-registration/user-registration.component.ts
@@ -64,4 +64,20 @@ export class UserRegistrationComponent {
     };
   }
 
+  check_duplicate_email() {
+    const email = this.form.get('email').value;
+
+    this.userService.check_duplicate_email(email).subscribe({
+      next: (response) => {
+        console.log(response.msg)
+        this.form.get('email').setErrors(null);
+      },
+      error: (response) => {
+        const message = response.error.msg;
+        console.log(message);
+        this.form.get('email').setErrors({ duplicateEmail: true })
+      }
+    })
+  }
+
 }
